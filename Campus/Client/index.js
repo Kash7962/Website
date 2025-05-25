6fa20f0080f09efc870d3675cbb41cce3435cb5a
Adi <adi.kumar8415@gmail.com>
2025-05-25 18:42:46 +0530

    Server changed

--- Campus/Client/index.js
+++ Campus/Client/index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
 })
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
