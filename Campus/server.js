0712610ac0e7f9d9e2baa901d94d94ee6cc7cc89
Subhankar Pandit <subhankar.pandit2002@gmail.com>
2025-05-31 13:03:56 +0100

    Views Public Routes defined

---
+++ Campus/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+const path = require('path');
+
+mongoose.connect(process.env.DB_URL,{});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.send("");
+})
+app.get('/Academic/Achievements', (req, res) => {
+  res.render('Academic/Achievements');
+});
+app.get('/Academic/CampusLife', (req, res) => {
+  res.render('Academic/CampusLife');
+});
+app.get('/Academic/Convocation', (req, res) => {
+  res.render('Academic/Convocation');
+});
+app.get('/Academic/Courses', (req, res) => {
+  res.render('Academic/Courses');
+});
+app.get('/Academic/Examination', (req, res) => {
+  res.render('Academic/Examination');
+});
+app.get('/Academic/FACILITIES', (req, res) => {
+  res.render('Academic/FACILITIES');
+});
+
+
+
+app.get('/WhoWeAre/AboutKASH-LOGO', (req, res) => {
+  res.render('WhoWeAre/AboutKASH-LOGO');
+});
+app.get('/WhoWeAre/AboutSOLID_KASH', (req, res) => {
+  res.render('WhoWeAre/AboutSOLID_KASH');
+});
+app.get('/WhoWeAre/Aboutus1', (req, res) => {
+  res.render('WhoWeAre/Aboutus1');
+});
+app.get('/WhoWeAre/Faculty', (req, res) => {
+  res.render('WhoWeAre/Faculty');
+});
+app.get('/WhoWeAre/Government', (req, res) => {
+  res.render('WhoWeAre/Government');
+});
+app.get('/WhoWeAre/KASHEXECUTIVE', (req, res) => {
+  res.render('WhoWeAre/KASHEXECUTIVE');
+});
+app.get('/WhoWeAre/PrincipalsDesk', (req, res) => {
+  res.render('WhoWeAre/PrincipalsDesk');
+});
+app.get('/WhoWeAre/Vision&Mission', (req, res) => {
+  res.render('WhoWeAre/Vision&Mission');
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+})
